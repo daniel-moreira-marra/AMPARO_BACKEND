87b97b07f08ce80c941929d85f14f1fab558e6ad
Euler <eulerborgesf@gmail.com>
2026-03-14 18:50:05 -0300

    fix(6): pipeline ci/cd

--- core/views/healthcheck.py
+++ core/views/healthcheck.py
@@ -1,9 +1,11 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from core.docs import healthcheck_docs
 from core.exceptions.responses import success_response
 
 class HealthCheckView(APIView):
+    permission_classes = [AllowAny]
     """
     Endpoint simples para o frontend testar se a API está ativa.
     Não exige autenticação.
