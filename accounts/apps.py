dc1a615142c08f6ab439f7b571cf499e61df0e12
danielmoreiramarra <danielmoreiramarra@gmail.com>
2026-05-14 17:51:51 -0300

    Deploy do Backend com ajuste em accounts/apps.py e events.py.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,14 +1,16 @@
 from django.apps import AppConfig
-
 from core.events import register_handler
-
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
 
     def ready(self):
-        import accounts.events  # Importa os handlers para que sejam registrados
-        register_handler("user_registered", user_registered_handler)
-        register_handler("user_profile_updated", user_profile_updated_handler)
-        register_handler("user_password_changed", user_password_changed_handler)
+        # 1. Importamos o módulo inteiro.
+        # Agora o Python sabe que 'accounts.events' existe neste escopo.
+        import accounts.events 
 
+        # 2. Usamos o prefixo do módulo para referenciar as funções!
+        register_handler("user_registered", accounts.events.user_registered_handler)
+        register_handler("user_profile_updated", accounts.events.user_profile_updated_handler)
+        register_handler("user_password_changed", accounts.events.user_password_changed_handler)
+        register_handler("password_reset_requested", accounts.events.password_reset_requested_handler)
