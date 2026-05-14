5257cf18ff386ff6c98316454e8131a741e5cc03
danielmoreiramarra <danielmoreiramarra@gmail.com>
2026-05-14 17:43:07 -0300

    Deploy do Backend com ajuste em accounts/apps.py.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,14 +1,13 @@
 from django.apps import AppConfig
 
 from core.events import register_handler
-from .events import user_registered_handler, user_profile_updated_handler, user_password_changed_handler
 
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
 
     def ready(self):
-        
+        import accounts.events  # Importa os handlers para que sejam registrados
         register_handler("user_registered", user_registered_handler)
         register_handler("user_profile_updated", user_profile_updated_handler)
         register_handler("user_password_changed", user_password_changed_handler)
